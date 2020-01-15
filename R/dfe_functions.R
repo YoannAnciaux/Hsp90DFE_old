3cc07950d89696020d66b4a4d90721ecd5e20a84
Yoann Anciaux <anciaux.yoann@gmail.com>
2020-01-15 16:54:49 +0100

    DFE Tenaillon function, test, doc
    Creates the function which returns the density of the first equation in the paragraph *"Distribution of
    mutation effects"* in Tenaillon (2014). The equation is adapted to show the same parameters as Martin & Lenormand (2015) plus the parameters \code{Q} and \code{alpha}.
    
    Function implemented using the package gsl
    Test check only if :
    - density = 0 if s > so
    - density > 0 if s <= so
    
    Sources : https://doi.org/10.1146/annurev-ecolsys-120213-091846

--- R/dfe_functions.R
+++ R/dfe_functions.R
@@ -48,3 +48,83 @@ dfgm_martin <- function(s, n, lambda, so) {
   return(distr::d(S)(s))
 }
 
+#' DFE of FGM from Tenaillon (2014).
+#'
+#' Density of the distribution of fitness effects of random mutations from
+#' \href{https://doi.org/10.1146/annurev-ecolsys-120213-091846}{Tenaillon (2014)}
+#' adapted to show the same parameters as Martin & Lenormand (2015) plus the
+#' parameters \code{Q} and \code{alpha}.
+#' The density corresponds to the first equation in the paragraph *"Distribution of
+#' mutation effects"* in the article.
+#' The density is encoded using the functions from the package \code{\link[gsl]{gsl}}
+#'
+#' @inheritParams dfgm_martin
+#' @param alpha Positive real number. Scaling parameter in the formulation of
+#' Tenaillon et al. (2007). (Identifiability problem with \code{lambda})
+#' @param Q Positive real number. Parameter that modifies the concavity of the
+#' fitness decline from the optimum.
+#'
+#' @return The density for each quantiles (selection coefficients) in a vector
+#' format. The length of the vector is equal to the length of \code{s}.
+#'
+#' @examples
+#' s <- sample(seq(-2, 1.5, 0.1))
+#' fs <- dfgm_tenaillon(s, 2, 0.1, 1, 1/2, 2)
+#'
+#' @source
+#' \itemize{
+#'   \item Tenaillon, O. (2014). The utility of Fisher's geometric model in
+#'   evolutionary genetics. Annual review of ecology, evolution, and systematics,
+#'   45, 179-201.
+#'   \item Martin, G., & Lenormand, T. (2015). The fitness effect of mutations
+#'   across environments: Fisher's geometrical model with multiple optima.
+#'   Evolution, 69(6), 1433-1447.
+#'   \item Tenaillon, O., Silander, O. K., Uzan, J. P., & Chao, L. (2007).
+#'   Quantifying organismal complexity using a population genetic approach.
+#'   PloS one, 2(2), e217.
+#' }
+#'
+#'@export
+dfgm_tenaillon <- function(s, n, lambda, so, alpha, Q) {
+
+  #### Arguments checks ####
+  coll <- checkmate::makeAssertCollection()
+  checkmate::assert_numeric(s, finite = T, any.missing = F, null.ok = F,
+                            add = coll)
+  checkmate::assert_count(n, na.ok = F, positive = T, null.ok = F, add = coll)
+  checkmate::assert_number(lambda, na.ok = F, lower = .Machine$double.eps,
+                           finite = T, null.ok = F, add = coll)
+  checkmate::assert_number(so, na.ok = F, finite = T, null.ok = F,
+                           add = coll)
+  checkmate::assert_number(alpha, na.ok = F, lower = .Machine$double.eps,
+                           finite = T, null.ok = F, add = coll)
+  checkmate::assert_number(Q, na.ok = F, lower = .Machine$double.eps,
+                           finite = T, null.ok = F, add = coll)
+  checkmate::reportAssertions(coll)
+
+  #### Density ####
+  # The density is 0 is s > so, however the function gsl::bessel_In produces
+  # NA/Nan/Inf when used with s > so. To overcome this numerical issue we compute
+  # the density only for the values of s <= so and set the density to 0 for the
+  # other.S
+  d <- numeric(length(s))
+  idx_s_infeqso <- which(s <= so)
+  s <- s[idx_s_infeqso]
+
+  # The formula from Tenaillon (2014) is separated in the for multiplicative terms
+  # A B C D for readibility.
+  A <- exp( -(((-s + so) / alpha)^(2/Q) + (so / alpha)^(2/Q)) / (2 * lambda) ) / (alpha * Q * lambda)
+  B <- ((-s + so) / alpha)^((n/2 + 1 - Q)/Q)
+  C <- (so / alpha)^((1 - n/2) / Q)
+  D <- gsl::bessel_In(n = n/2 - 1, x = 1/lambda * ((-s + so) / alpha)^(1/Q) * (so / alpha)^(1/Q))
+  d_s_infeqso <- A*B*C*D
+  d[idx_s_infeqso] <- d_s_infeqso
+  return(d)
+}
+
+
+
+
+
+
+
